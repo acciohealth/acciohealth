29e44bb6908da4bbf85dc5c7a333c358030e0536
Anand Naik <anand.naik@flipkarts-MacBook-Pro-5.local>
2016-10-02 19:09:52 +0530

    Blog posts fix done

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -14,11 +14,12 @@ class WelcomeController extends Controller
 
         $post_images = array();
 
-        $blog_posts = DB::connection('mysql_wp')->table('wp_posts')->where('post_type', 'post')->where('post_status', 'publish')->orderBy('post_date', 'DESC')->take(3)->get();
+        $blog_posts = DB::connection('mysql_wp')->table('wp_posts')->where('post_type', 'post')->where('post_status', 'publish')->orderBy('post_date', 'DESC')->distinct('ID')->take(3)->get();
 
         foreach($blog_posts as $key => $blog_post){
-            $post = DB::connection('mysql_wp')->table('wp_posts')->where('post_type', 'attachment')->where('post_status', 'inherit')->where('post_parent', $blog_post->ID)->orderBy('post_date', 'DESC')->first();
-            $post_images[$key] = $post->guid;
+            $post_images[$key] = DB::connection('mysql_wp')->table('wp_posts')->where('post_parent', $blog_post->ID)->where('post_type', 'attachment')->where('post_status', 'inherit')->value('guid');
+            if($post_images[$key] == null)
+                $post_images[$key] = 'No image available';
         }
 
         return view('welcome', ['blog_posts' => $blog_posts, 'post_images' => $post_images]);
